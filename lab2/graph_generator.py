c6162a8dc64db0c5e5741eb9fbcf9fcac0266559
Michał Nożkiewicz <michno513@gmail.com>
2023-03-17 01:47:05 +0100

    lab2_task3

--- lab2/graph_generator.py
+++ lab2/graph_generator.py
@@ -2,27 +2,44 @@ import networkx as nx
 import numpy as np
 
 
-def add_edge_weights(g):
+class Edge:
+    def __init__(self, a, b, resistance, voltage, index):
+        self.a = a
+        self.b = b
+        self.resistance = resistance
+        self.voltage = voltage
+        self.index = index
+        self.current = 0
+
+
+def generate_circuit(g):
+    n = g.number_of_nodes()
+    s = np.random.randint(0, n - 1)
+    t = np.random.randint(s + 1, n)
+
+    index = 0
     for a, b in g.edges:
-        g[a][b]['weight'] = np.round(np.random.uniform(5, 10), 2)
+        if (a, b) not in [(s, t), (t, s)]:
+            resistance = max(np.round(np.random.rand(), 2), 0.01)
+            g[a][b]['edge'] = Edge(a, b, resistance, 0, index)
+            g[b][a]['edge'] = g[a][b]['edge']
+            index += 1
 
-
-def find_source_and_target(g):
-    for a in range(g.number_of_nodes()):
-        for b in range(g.number_of_nodes() - 1, a, -1):
-            if a not in g.neighbors(b):
-                return a, b
-    return 0, g.number_of_nodes() - 1
+    e = max(np.round(np.random.rand(), 2), 0.01)
+    g.add_edge(s, t)
+    g[s][t]['edge'] = Edge(s, t, 0, e, index)
+    g[t][s]['edge'] = g[s][t]['edge']
+    return s, t, e
 
 
 def generate_erdos_renyi(n, p):
     g = nx.erdos_renyi_graph(n, p)
-    g = nx.convert_node_labels_to_integers(g, first_label=0)
     while not nx.is_connected(g):
         g = nx.erdos_renyi_graph(n, p)
-    add_edge_weights(g)
-    s, t = find_source_and_target(g)
-    E = np.round(np.random.uniform(5, 10), 2)
-    g.add_edge(s, t, weight=E)
-    return g, s, t, E
+    g = nx.convert_node_labels_to_integers(g, first_label=0)
+    s, t, e = generate_circuit(g)
+    return g, s, t, e
+
+
+# g, s, t, e = generate_erdos_renyi(5, 0.5)
 
--- lab2/graph_visualiser.py
+++ lab2/graph_visualiser.py
@@ -3,40 +3,44 @@ import networkx as nx
 import numpy as np
 import graph_generator
 
-
-def draw_undirected_with_resistance(g, s, t, E):
+def draw_resistance(g, s, t, small=True):
+    n = g.number_of_nodes()
     plt.figure(figsize=(10, 8))
     pos = nx.kamada_kawai_layout(g)
 
     labels = {}
-    for i in range(g.number_of_nodes()):
+    for i in range(n):
         labels[i] = str(i)
-    nx.draw_networkx_labels(
-        g,
-        pos,
-        labels,
-        font_size=10,
-        font_color="yellow"
-    )
+
+    if small:
+        nx.draw_networkx_labels(
+            g,
+            pos,
+            labels,
+            font_size=10,
+            font_color="black"
+        )
+
+    node_size = 30 if small else 10
     nx.draw_networkx_nodes(
         g,
         pos,
-        nodelist=[i for i in range(g.number_of_nodes()) if i != s and i != t],
-        node_color="blue",
-        node_size=30,
+        nodelist=[i for i in range(n)],
+        node_color="red",
+        node_size=node_size,
     )
 
     nx.draw_networkx_nodes(
         g,
         pos,
         nodelist=[s, t],
-        node_color="red",
+        node_color="blue",
         node_size=30,
     )
     label_weights = {}
     for a, b in g.edges:
-        label_weights[(a, b)] = str(g[a][b]['weight'])
-    label_weights[(s, t)] = '\u03B5= ' + str(g[s][t]['weight'])
+        label_weights[(a, b)] = str(g[a][b]['edge'].resistance)
+    label_weights[(s, t)] = '\u03B5= ' + str(g[s][t]['edge'].voltage)
 
     nx.draw_networkx_edges(
         g,
@@ -51,12 +55,85 @@ def draw_undirected_with_resistance(g, s, t, E):
         edgelist=[(s, t)],
         width=0.5,
         alpha=0.5,
-        edge_color="red"
+        edge_color="blue"
     )
-    nx.draw_networkx_edge_labels(
-        g,
+    if small:
+        nx.draw_networkx_edge_labels(
+            g,
+            pos,
+            edge_labels=label_weights
+        )
+    plt.show()
+
+
+def draw_current(g, s, t, small=True):
+    n = g.number_of_nodes()
+    g2 = nx.DiGraph()
+
+    labels = {}
+    for i in range(n):
+        labels[i] = str(i)
+
+    label_weights = {}
+    for a, b in g.edges:
+        edge = g[a][b]['edge']
+        if a < b and edge.current < 0 or a > b and edge.current > 0:
+            a, b = b, a
+        g2.add_edge(a, b)
+        label_weights[(a, b)] = str(np.round(abs(edge.current), 2))
+
+    label_weights[(s, t)] = '\u03B5= ' + str(g[s][t]['edge'].voltage)
+    plt.figure(figsize=(10, 8))
+    pos = nx.kamada_kawai_layout(g2)
+    if small:
+        nx.draw_networkx_labels(
+            g2,
+            pos,
+            labels,
+            font_size=5,
+            font_color="black"
+        )
+
+    node_size = 30 if small else 10
+    nx.draw_networkx_nodes(
+        g2,
         pos,
-        edge_labels=label_weights
+        nodelist=[i for i in range(n)],
+        node_color="red",
+        node_size=node_size,
     )
+
+    nx.draw_networkx_nodes(
+        g2,
+        pos,
+        nodelist=[s, t],
+        node_color="blue",
+        node_size=30,
+    )
+
+    arrowsize = 18 if small else 5
+    nx.draw_networkx_edges(
+        g2,
+        pos,
+        edge_color='red',
+        edgelist=[(a, b) for a, b in g2.edges if a not in [s, t] or b not in [s, t]],
+        arrowsize=arrowsize
+    )
+
+    edgelist = [(s, t)] if (s, t) in g2.edges else [(t, s)]
+    nx.draw_networkx_edges(
+        g2,
+        pos,
+        edgelist=edgelist,
+        edge_color='blue',
+        arrows=False
+    )
+
+    if small:
+        nx.draw_networkx_edge_labels(
+            g,
+            pos,
+            edge_labels=label_weights
+        )
     plt.show()
 
